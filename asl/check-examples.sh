23f683bf703d6c0f0b97c94b3737ae8a85281dd0
Unknown <mbdavid297@gmail.com>
2018-05-30 12:22:26 +0200

    salu2

--- asl/check-examples.sh
+++ asl/check-examples.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# echo "BEGIN examples-initial/typecheck"
-# for f in ../examples/jpbasic_chkt_*.asl; do
-#     echo $(basename "$f")
-#     ./asl "$f" | egrep ^L > tmp.err
-#     diff tmp.err "${f/asl/err}"
-#     rm -f tmp.err
-# done
-# echo "END   examples-initial/typecheck"
+echo "BEGIN examples-initial/typecheck"
+for f in ../examples/jpbasic_chkt_*.asl; do
+    echo $(basename "$f")
+    ./asl "$f" | egrep ^L > tmp.err
+    diff tmp.err "${f/asl/err}"
+    rm -f tmp.err
+done
+echo "END   examples-initial/typecheck"
 
-# echo ""
-# echo "BEGIN examples-full/typecheck"
-# for f in ../examples/jp_chkt_*.asl; do
-#     echo $(basename $f)
-#     ./asl $f | egrep ^L > tmp.err
-#     if ! (diff tmp.err ${f/asl/err}) &>/dev/null; then
-#     >&2
-#     (diff tmp.err ${f/asl/err})
-#     exit 1 
-#     fi
-#     rm -f tmp.err
-# done
-# echo "END   examples-full/typecheck"
+echo ""
+echo "BEGIN examples-full/typecheck"
+for f in ../examples/jp_chkt_*.asl; do
+    echo $(basename $f)
+    ./asl $f | egrep ^L > tmp.err
+    if ! (diff tmp.err ${f/asl/err}) &>/dev/null; then
+    >&2
+    (diff tmp.err ${f/asl/err})
+    exit 1 
+    fi
+    rm -f tmp.err
+done
+echo "END   examples-full/typecheck"
 
 # echo ""
 # echo "BEGIN examples-initial/codegen"
